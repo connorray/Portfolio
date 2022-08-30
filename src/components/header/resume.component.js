6ef738f4e52b1d659a2eb6b8cf1df84ac8b1a86e
Raymond Yoo <rayyoo4223@gmail.com>
2022-08-29 21:39:21 -0400

    update resume on portfolio

--- src/components/header/resume.component.js
+++ src/components/header/resume.component.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Pdf from "./RaymondYoo.pdf";
+import UpdatedPDF from "./Ray2022";
 
 export const Download = () => {
   return (
     <>
       <a
         className="banner-button btn btn-outline-dark"
-        href={Pdf}
+        href={UpdatedPDF}
         target="_blank"
       >
         Resume
